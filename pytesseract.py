c29c9c0347cb64733f29a082d5762adee4235ae1
Saaida Hnais <120813264+saaida1@users.noreply.github.com>
2024-01-12 13:59:14 +0100

    pytesseract.py

--- pytesseract.py
+++ pytesseract.py
@@ -2,35 +2,32 @@ import pytesseract
 import os
 from PIL import Image, ImageFilter, ImageOps
 
-# Define the path to tessaract.exe
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\SunShine\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-# Define the path to image
-os.environ['TESSDATA_PREFIX'] = r'C:\Users\SunShine\AppData\Local\Programs\Tesseract-OCR\tessdata'
+def extract_text_from_image(image_path):
+    # Define the path to tessaract.exe
+    pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-# Open the image using PIL
-img = Image.open('tale.webp')
+    # Define the path to image
+    os.environ['TESSDATA_PREFIX'] = 'C:/Program Files/Tesseract-OCR/tessdata'
 
-# Set conf of the OCR Engine Mode & Page Segmentation Mode
-config: str = ('-l ara+fra+eng --oem 1 --psm 3')
+    # Open the image using PIL
+    img = Image.open(image_path)
 
-# Convert to grayscale
-img = img.convert('L')
+    # Set conf of the OCR Engine Mode & Page Segmentation Mode
+    config: str = ('-l ara+fra+eng --oem 1 --psm 3')
 
-# Enhance the image to improve its quality.
-img = ImageOps.autocontrast(img)
+    # Convert to grayscale
+    img = img.convert('L')
 
-# remove noise
-img = img.filter(ImageFilter.MedianFilter())
+    # Enhance the image to improve its quality.
+    img = ImageOps.autocontrast(img)
 
-# Apply a filter if the colors of the text and the background are not optimal for OCR
-img = ImageOps.invert(img)  
+    # remove noise
+    img = img.filter(ImageFilter.MedianFilter())
 
-text = pytesseract.image_to_string(img, lang='ara+fra+eng')
+    # Apply a filter if the colors of the text and the background are not optimal for OCR
+    img = ImageOps.invert(img)
 
-# Open a new file and write the extracted text to it
-with open('output.txt', 'w', encoding='utf-8') as file:
-    file.write(text)
+    text = pytesseract.image_to_string(img, lang='ara+fra+eng')
 
-# Print the extracted text
-print(text)
+    return text
